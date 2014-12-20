e1468a65644d5d2ac92128c1cf4980583a2fa136
truong-d <truongdq54@gmail.com>
2014-12-20 10:24:02 +0900

    fix install_syncd.sh

--- install_syncd.sh
+++ install_syncd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p .syncd
-cd .syncd
+mkdir -p ~/.syncd
+cd ~/.syncd
 git clone https://github.com/truongdq54/syncd.git
-cd syncd
+cd ~/.syncd
 sudo ln -s $PWD/syncd /usr/local/bin/syncd
