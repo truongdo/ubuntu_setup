c4cce0f3d5ce36fabb99588120a8929ba08c53c9
truong-d <truongdq54@gmail.com>
2014-12-20 10:32:56 +0900

    fix install_syncd

--- install_syncd.sh
+++ install_syncd.sh
@@ -3,6 +3,6 @@ cd ~/.syncd
 git clone https://github.com/truongdq54/syncd.git || echo "already exist"
 cd ~/.syncd
 if [[ -f /usr/local/bin/syncd ]]; then
-    rm /usr/local/bin/syncd
+    sudo rm /usr/local/bin/syncd
 fi
 sudo ln -s $PWD/syncd /usr/local/bin/syncd
